d564f680f443b0e2f1e26060b430fee2efb9d36a
RafaBaez <96098524+RafaBaez@users.noreply.github.com>
2021-12-13 21:47:29 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("A+B")
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Happy)
+basic.showString("RAFA")
